249b878b02c94e622314c09c6ae3d397249cf5fa
Steven Gomez <steve_gomez@usa.net>
2019-07-16 13:21:03 -0500

    Create MythXbuntu.sh
    Added first few commands to install MythTV from PPA

---
+++ MythTV/MythXbuntu.sh
@@ -0,0 +1,9 @@
+
+# Script to install Myth TV on Xubuntu 18.04
+#
+# Starting point for installation can be found here:
+# http://www.mythbuntu.org/home/news
+#
+sudo add-apt-repository ppa:mythbuntu/30
+sudo apt-get update
+
