0d18c5cdac5f48fb1db2003b6e52826b1ea40749
Steven Gomez <steve_gomez@usa.net>
2019-07-23 16:20:28 -0500

    Tweaks for certificate installation

--- CentOS_7/C7_InstallLAMPServer.sh
+++ CentOS_7/C7_InstallLAMPServer.sh
@@ -78,8 +78,14 @@ function AddLocalHostNames
 ##########################################################################
 #
 function InstallEncryptClient
+# https://certbot.eff.org/lets-encrypt/centosrhel7-apache
 {
     echo "Function: InstallEncryptClient starting"
+    
+    # Red Hat installations require these commands 
+    # TODO - NOTE that REGION must be updated with the appropriate Amazon Elastic Compute Cloud (EC2) region
+    # yum -y install yum-utils
+    # yum-config-manager --enable rhui-REGION-rhel-server-extras rhui-REGION-rhel-server-optional
 
     yum -y install certbot python2-certbot-apache mod_ssl
 
@@ -87,9 +93,14 @@ function InstallEncryptClient
     systemctl stop httpd
     
     # TODO: Add code to get the certificate
+    certbot --apache
+    # certbot certonly --apache # <- Run this if manual Apache configuration is needed
+    
     # TODO: Add code to restart apache
-
-
+    systemctl start httpd
+    
+    # Set the default crontab to automatically renew the certificate
+    echo "0 0,12 * * * root python -c 'import random; import time; time.sleep(random.random() * 3600)' && certbot renew" | sudo tee -a /etc/crontab > /dev/null
 
     echo "Function: InstallEncryptClient complete"
 }
