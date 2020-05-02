f515394ce2ed24aec8ff9d12b003c3729d2dc491
Steven Gomez <steve_gomez@usa.net>
2020-05-02 12:57:55 -0500

    Adding framework to script to install RHEL Ansible Tower on C8S. Also simplified file names of other scripts.

--- CentOS_8/Ansible.sh
+++ CentOS_8/Ansible.sh
@@ -1,10 +1,147 @@
 #!/bin/bash
 
-dnf -y install epel-release
-dnf makecache
-dnf install ansible
+# https://releases.ansible.com/ansible-tower/setup-bundle/ansible-tower-setup-bundle-3.6.4-1.el8.tar.gz
+TOWER_VER=3.6.4-1
+OS=el8
+EXT=.tar.gz
+TOWER_PKG=ansible-tower-setup-bundle-${TOWER_VER}.${OS}${EXT}
+TOWER_URL=https://releases.ansible.com/ansible-tower/setup-bundle/${TOWER_PKG}
 
-ansible --version
+
+# ===================================================================
+# Note from RHEL Installation Notes:
+# PackageKit can frequently interfere with the installation/update mechanism. 
+# Consider disabling or removing PackageKit if installed prior to running the 
+# setup process.
+function DisablePackageKit
+{
+    echo 'Disabling PackageKit'
+
+    systemctl stop packagekit
+}
+# -------------------------------------------------------------------
+
+# ===================================================================
+#
+function PerformUpdate
+{
+    dnf -y update
+}
+# -------------------------------------------------------------------
+
+function InstallEpel
+{
+    dnf -y install epel-release
+    dnf makecache
+}
+# -------------------------------------------------------------------
+
+# ===================================================================
+# Ansible Tower uses PostgreSQL 10, which is an SCL package on RHEL 7 
+# and an app stream on RHEL8. 
+#
+# Note that the Tower package includes a matching Ansible installation
+#
+function InstallAnsible
+{
+    dnf install ansible
+    ansible --version
+}
+# -------------------------------------------------------------------
+
+# ===================================================================
+# https://manjaro.site/how-to-postgresql-10-on-centos-8/
+#
+function InstallPostgreSQL
+{
+    echo 'InstallPostgreSQL'
+
+    # Lists postgresql modules available ([d] indicates the default value
+    dnf module list postgresql
+
+    # Install Version 10 explicitely
+    dnf -y install @postgresql:10
+
+    # The following would install the default version without specifying
+    # dnf -y install postgresql
+
+    # Install contrib package (may not be mandatory)
+    dnf -y install postgresql-contrib
+
+    # NOTE: The following are post install steps
+    # postgresql-setup initdb
+    #
+    # systemctl start postgresql
+    # systemctl enable postgresql
+    #
+    # su - postgres
+    # psql
+    # ALTER USER postgres WITH PASSWORD '12345';
+    #
+    # su
+    # edit /var/lib/pgsql/data/postgresql.conf
+    # 
+    # Edit the value of listen_addresses = ‘localhost‘ to ‘*’ as follow:
+    # 
+    # - Connection Settings -
+    #  listen_addresses = '*'          # what IP address(es) to listen on;
+    # 
+    # su
+    # edit /var/lib/pgsql/data/pg_hba.conf
+    # 
+    # add the following line to the end of the file
+    # 
+    # host    all             all             0.0.0.0/0               md5
+    # 
+    # Restart PostgreSQL service
+    # 
+    # su
+    # systemctl restart postgresql
+    # 
+
+}
+# -------------------------------------------------------------------
+
+# ===================================================================
+#
+function InstallTower
+{
+    wget ${TOWER_URL}
+
+}
+# -------------------------------------------------------------------
 
 
+
+# ===================================================================
+#
+function MyFunction
+{
+    echo 'Function sample'
+}
+# -------------------------------------------------------------------
+
+
+# ====================================================================================
+# ====================================================================================
+# ====================================================================================
+#
+# Script execution begins here
+#
+# ====================================================================================
+
+##########################################################################
+# NOTE - First must stop PackageKit or you will hang until it times out
+#        which is a really, really long time.
+
+
+DisablePackageKit
+PerformUpdate
+InstallEpel
+
+## InstallAnsible   # <= Included in Tower package
+
+InstallPostgreSQL
+
+InstallTower
 
