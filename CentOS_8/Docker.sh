650969afbb34692ac36905eec6e09bc664ce0972
Steven Gomez <steve_gomez@usa.net>
2020-05-27 17:19:30 -0500

    Updated per changes on docs.docker.com
    Changed to function format to allow including function for Kubernetes installation.

--- CentOS_8/Docker.sh
+++ CentOS_8/Docker.sh
@@ -7,33 +7,62 @@
 #      and CentOS 8 specific yum repos are not yet available either
 #      at the time of this writing - 20191017
 #
-# Seems to install OK, but will proceed with caution
+#      Made updates 20200527 based on changes posted https://docs/docker.com
 #
 ####################################################
+function InstallDocker
+{
+    yum -y erase docker*
 
-# From page 28
-cat >/etc/yum.repos.d/docker.repo <<-EOF
-[dockerrepo]
-name=Docker Repository
-baseurl=https://yum.dockerproject.org/repo/main/centos/7
-enabled=1
-gpgcheck=1
-gpgkey=https://yum.dockerproject.org/gpg
-EOF
+    # From page 28, but obsolete because the repo locations have changed 
+    #cat >/etc/yum.repos.d/docker.repo <<-EOF
+    #[dockerrepo]
+    #name=Docker Repository
+    #baseurl=https://yum.dockerproject.org/repo/main/centos/7
+    #enabled=1
+    #gpgcheck=1
+    #gpgkey=https://yum.dockerproject.org/gpg
+    #EOF
 
-yum erase docker*
-yum -y install docker-engine
+    # Update from docker docs
+    yum -y install yum-utils
+    yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
-# Start the Docker daemon (in the foreground)
-docker daemon -H unix:///var/run/docker.sock -H tcp://0.0.0.0:2375
-# Control C to kill it
+    yum install docker-ce docker-ce-cli containerd.io
+    # yum -y install docker-engine
 
-# Start as a service
-systemctl start docker
+    # Start the Docker daemon (in the foreground, as a test)
+    # docker daemon -H unix:///var/run/docker.sock -H tcp://0.0.0.0:2375
+    # Control C to kill it
 
-# Enable start at boot
-systemctl enable docker
+    # Start as a service
+    systemctl start docker
 
+    # Enable start at boot
+    systemctl enable docker
+}
+
+function InstallKubernetes
+{
+
+}
+
+# ====================================================================================
+# ====================================================================================
+# ====================================================================================
+#
+# Script execution begins here
+#
+# ====================================================================================
+
+##########################################################################
+# NOTE - First must stop PackageKit or you will hang until it times out
+#        which is a really, really long time.
+systemctl stop packagekit
+
+PerformUpdate
+InstallDocker
+Install Kubernetes
 
 
 
