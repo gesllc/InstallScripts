1455503dcb64a47c59e5b8e7e7eedbdf8a2002a0
Steven Gomez <steve_gomez@usa.net>
2020-05-28 15:05:02 -0500

    Docker install still problematic due to repository conflicts
    Now attempting curl from docker.com

--- CentOS_7/Kubernetes.sh
+++ CentOS_7/Kubernetes.sh
@@ -8,41 +8,34 @@ function PerformUpdate
 
 # ====================================================================================
 function InstallDocker
-}
-    # Create repo for docker-ce installation
-    cat <<EOF > /etc/yum.repos.d/centos.repo
-    [centos]
-    name=CentOS-7
-    baseurl=http://ftp.heanet.ie/pub/centos/7/os/x86_64/
-    enabled=1
-    gpgcheck=1
-    gpgkey=http://ftp.heanet.ie/pub/centos/7/os/x86_64/RPM-GPG-KEY-CentOS-7
-    #additional packages that may be useful
-    [extras]
-    name=CentOS-$releasever - Extras
-    baseurl=http://ftp.heanet.ie/pub/centos/7/extras/x86_64/
-    enabled=1
-    gpgcheck=0
-    EOF
+{
+    # Remove previously installed versions (if any)
+    yum -y remove docker*
     
-    yum -y install docker
+    # Install docker using curl
+    curl -fsSl https://get.docker.com/ | sh
+    
+    # Setup standard user access
+    usermod --append --groups docker developer
+    
     systemctl enable --now docker
-
 }
 
 # ====================================================================================
 function InstallKubernetes
 {
-    cat <<EOF > /etc/yum.repos.d/kubernetes.repo
-    [kubernetes]
-    name=Kubernetes
-    baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
-    enabled=1
-    gpgcheck=1
-    repo_gpgcheck=1
-    gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-    EOF
-    
+# NOTE: here-document cannot start with spaces or tabs!    
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+# End of here-document
+
     setenforce 0
     
     #vi /etc/selinux/config
