cf87eb781279afcc74e1090b12e0eac6b7692a6d
Steven.Gomez <steve.gomez@biomerieux.com>
2020-05-28 08:16:29 -0500

    Modifications to Docker.sh intended to workaround installation verson conflicts

--- CentOS_8/Docker.sh
+++ CentOS_8/Docker.sh
@@ -12,7 +12,13 @@
 ####################################################
 function InstallDocker
 {
-    yum -y erase docker*
+    # https://www.techrepublic.com/article/a-better-way-to-install-docker-on-centos-8/
+    # The link above shows workaround for mismatch encountered with containerd.io
+    #
+    # See also:
+    # https://thenewstack.io/how-to-install-a-kubernetes-cluster-on-red-hat-centos-8/
+
+    dnf -y erase docker*
 
     # From page 28, but obsolete because the repo locations have changed 
     #cat >/etc/yum.repos.d/docker.repo <<-EOF
@@ -24,26 +30,89 @@ function InstallDocker
     #gpgkey=https://yum.dockerproject.org/gpg
     #EOF
 
-    # Update from docker docs
-    yum -y install yum-utils
-    yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+    # Update from docker docs & other web sources (to work around containerd.io version issues)
 
-    yum install docker-ce docker-ce-cli containerd.io
-    # yum -y install docker-engine
+    # Install (older version) docker
+    dnf -y install docker-ce-3:18.09.1-3.el7
 
-    # Start the Docker daemon (in the foreground, as a test)
-    # docker daemon -H unix:///var/run/docker.sock -H tcp://0.0.0.0:2375
-    # Control C to kill it
+    # Setup firewall ports
+    firewall-cmd --permanent --add-port=6443/tcp
+    firewall-cmd --permanent --add-port=2379-2380/tcp
+    firewall-cmd --permanent --add-port=10250/tcp
+    firewall-cmd --permanent --add-port=10251/tcp
+    firewall-cmd --permanent --add-port=10252/tcp
+    firewall-cmd --permanent --add-port=10255/tcp
+    firewall-cmd –reload
 
-    # Start as a service
-    systemctl start docker
+    modprobe br_netfilter
 
-    # Enable start at boot
-    systemctl enable docker
+    echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
+
+    # Start and enable Docker
+    systemctl enable --now docker
+
+    # Add standard user to Docker group
+    usermod --append --groups docker developer
+
+    # Setup repository, then install containerd.io package
+    dnf -y install https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm
+    dnf -y install docker-ce
+
 }
 
 function InstallKubernetes
 {
+
+    # These steps from:
+    # https://thenewstack.io/how-to-install-a-kubernetes-cluster-on-red-hat-centos-8/
+
+    # Setup repository for kubernetes
+    cat >/etc/yum.repos.d/kubernetes.repo <<-EOF
+    [kubernetes]
+    name=Kubernetes
+    baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+    enabled=1
+    gpgcheck=1
+    repo_gpgcheck=1
+    gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+    EOF
+
+    dnf install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+
+    systemctl enable --now kubelet
+
+    ## Manual steps now, but change to sed editing:
+    #nano /etc/sysctl.d/k8s.conf
+    #net.bridge.bridge-nf-call-ip6tables = 1
+    #net.bridge.bridge-nf-call-iptables = 1
+    #sysctl --system
+
+    ## Edit fstab
+    # nano /etc/fstab
+    # /dev/maper/cl-swap  <<-- Comment out this line
+
+    ## Log out of root, then as standard user << ??
+    # nano /etc/docker/daemon.json
+    # Then paste the following:
+    #{
+    #  "exec-opts": ["native.cgroupdriver=systemd"],
+    #  "log-driver": "json-file",
+    #  "log-opts": {
+    #    "max-size": "100m"
+    #  },
+    #  "storage-driver": "overlay2",
+    #  "storage-opts": [
+    #    "overlay2.override_kernel_check=true"
+    #  ]
+    #}
+
+    # mkdir --parents /etc/sstemd/system/docker.service.d
+
+    # systemctl daemon-reload
+    # systemctl restart docker
+    
+
+
 
 }
 
