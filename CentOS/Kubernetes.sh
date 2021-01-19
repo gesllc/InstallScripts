0c782c18a73b1c2b61aca15e094c0b69c3ed0887
Steven Gomez <steve_gomez@usa.net>
2021-01-19 12:59:10 -0600

    Added podman installation steps from RHEL support pages

--- CentOS/Kubernetes.sh
+++ CentOS/Kubernetes.sh
@@ -22,18 +22,28 @@ function InstallApplications
 #      The install steps here are from:
 #      https://podman.io/getting-started/installation
 #
-#      https://www.tecmint.com/create-local-yum-repository-on-centos-8/
-#
+#      Deprecated - https://www.tecmint.com/create-local-yum-repository-on-centos-8/
+#      https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/8/html/building_running_and_managing_containers/starting-with-containers_building-running-and-managing-containers
 function InstallDocker
 {
     # CentOS 8
-    sudo dnf -y module disable container-tools
-    sudo dnf -y install 'dnf-command(copr)'
-    sudo dnf -y copr enable rhcontainerbot/container-selinux
-    sudo curl -L -o /etc/yum.repos.d/devel:kubic:libcontainers:stable.repo \
-        https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/CentOS_8/devel:kubic:libcontainers:stable.repo
+    #sudo dnf -y module disable container-tools
+    #sudo dnf -y install 'dnf-command(copr)'
+    #sudo dnf -y copr enable rhcontainerbot/container-selinux
+    #sudo curl -L -o /etc/yum.repos.d/devel:kubic:libcontainers:stable.repo \
+    #    https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/CentOS_8/devel:kubic:libcontainers:stable.repo
 
-    sudo dnf -y install podman
+    #sudo dnf -y install podman
+    
+    # The following is from the Red Hat reference noted above:
+    yum module install -y container-tools
+    yum install -y podman-docker
+    
+    # Set up rootless container access
+    yum install slirp4netns podman -y
+    echo "user.max_user_namespaces=28633" > /etc/sysctl.d/userns.conf
+    sysctl -p /etc/sysctl.d/userns.conf
+        
 }
 
 # ====================================================================================
