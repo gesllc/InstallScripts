b7bd121c1b12003513b0b1354748a408bde0de8a
Steven Gomez <steve_gomez@usa.net>
2021-01-19 23:21:32 -0600

    More Ruby install changes

--- CentOS/Redmine.sh
+++ CentOS/Redmine.sh
@@ -54,8 +54,9 @@ function InstallRuby
         libyaml-devel libffi-devel openssl-devel make \
         bzip2 autoconf automake libtool bison sqlite-devel
 
-    curl -sSL https://rvm.io/mpapis.asc | gpg --import -
-    curl -L get.rvm.io | bash -s stable
+    curl -sSL https://rvm.io/mpapis.asc | gpg --import -  
+    curl -L https://get.rvm.io | bash -s stable
+    
     source /etc/profile.d/rvm.sh
     rvm reload
     rvm requirements run
