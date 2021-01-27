254311ca753f26c0ef8c23fd204aba33d3a39d95
Steven.Gomez <steve.gomez@biomerieux.com>
2021-01-26 19:11:42 -0600

    Removed dead comments from TeamCity.sh (now in README.md)

--- CentOS/TeamCity.sh
+++ CentOS/TeamCity.sh
@@ -2,65 +2,8 @@
 
 # 20201003 - Modified for CentOS 8 to reinstall on esximgmt
 #
-# Created base VM using:
-# 4 CPUs
-# 8 GB Ram
-# 35 GB hard drive - thin provisioned
-#
-# Perform minimal installation
-# 
-# After installation, shutdown and add HD for DB storage
-# 50 GB - Thick provisioned
-#
-# Create partition on drive as followws:
-# fdisk /dev/sdb
-# n (to create a new partition)
-# p (to make primary partition)
-# 1 (select partition 1)
-# Enter 1 to start format at cylinder 1, or enter first cylinder available
-# Enter (default to last cylinder to use all space)
-# w (write partition table & exit)
-
-# Format the drive partition created above
-# mkfs -t ext4 /dev/sdb1
-
-# Create mount point directory for MariaDB
-# mkdir /var/lib/mysql
-
-# Edit /etc/fstab & add:
-# /dev/sdb1 /var/lib/mysql ext4  defaults  1 1
-#
-# Reboot and confirm secondary drive is present using
-# df -h
-#
-# After adding the DB drive, shutdown to add third drive for TC data (artifacts)
-# 50 GB - thin provisioned.  Create partition & format similar to above.
-#
-# Create mount point directory for TCData
-# mkdir /opt/TCData
-#
-# Edit /etc/fstab & add:
-# /dev/sdc1 /opt/TCData ext4  defaults  1 1
-# 
-#
-# Post installation DB setup
-# mysql_secure_installation   <-- Assign root password (Cs..44), then answer Y to all questions
-#
-# mysql -u root -p       <-- Create database & access account for TeamCity
-# create database cidb character set UTF8 collate utf8_bin;
-# create user admin identified by 'firmware';
-# grant all privileges on cidb.* to admin;
-# grant process on *.* to admin;
-# quit; 
-# 
-
-# After running script and entering all DB configurations as above, log in as admin
-# and start TeamCity service using:
-# /opt/TeamCity/bin/teamcity-server.sh start
-#
-# Open a web browser:  http://<URL>:8111
-# Follow the directions in the browser.
-
+#            For proper installation, specific configuration steps are requred.
+#            See this repository's README.md for further details.
 
 ##########################################################################
 ##########################################################################
